6efb0ee3998015006d6c87320f08b1da33d4d68e
hichemsamti <samtihichem@gmail.com>
2021-07-13 13:28:10 +0100

    login log out

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -44,7 +44,7 @@ const userCtrl = {
 
         })
 
-        res.json({msg:"Register Success !"})
+        res.json({accesstoken})
 
        } catch (err) {
 
@@ -52,7 +52,86 @@ const userCtrl = {
 
        }
      
-     }
+     },
+
+
+   login: async (req,res) => {
+      
+      try {
+
+        const {email, password} = req.body
+
+        const user = await Users.findOne({email})
+
+        if(!user) return res.status(400).json({msg:"User does not exist"})
+
+        const isMatch= await bcrypt.compare(password, user.password)
+
+        if(!isMatch) return res.status(400).json({msg:"incorrect password"})
+
+        // if login success, create access and refresh token
+        const accesstoken = createAccessToken({id:user._id})
+        const refreshtoken =  createRefreshToken({id:user._id})
+
+        res.cookie("refreshtoken", refreshtoken, {
+
+            httpOnly:true,
+            path:"/user/refresh_token"
+
+        })
+
+        res.json({accesstoken})
+
+        res.json({msg: "Login Success"})
+
+      }catch(err){
+
+        return res.status(500).json({msg:err.message})
+
+      }
+
+   },
+
+
+   logout: async (req,res) => {
+
+       try {
+
+        res.clearCookie("refreshtoken",{ path : '/user/refresh_token' })
+
+        return res.json({msg:"Logged out"})
+
+
+
+       }catch(err){
+
+          return res.status(500).json({msg:err.message})
+       }
+
+   },
+
+   refreshToken:(req,res)=>{
+       try{
+
+        const rf_token = req.cookies.refreshtoken
+        if(!rf_token) return res.status(400).json({msg:"Please Login or Register"})
+          
+        jwt.verify(rf_token,process.env.REFRESH_TOKEN_SECRET,(err,user)=>{
+
+            if(err) return res.status(400).json({msg:"Please Login or Register"})
+            const accesstoken = createAccessToken({id:user.id})
+            res.json({accesstoken})
+        })
+    
+
+
+       }catch(err){
+
+           return res.status(500).json({msg:err.message})
+
+       }
+
+   }
 
 
 
