160971491f35b1f5c2ffb5e9469cf67b4c17ebd4
hichemsamti <samtihichem@gmail.com>
2021-07-18 21:49:18 +0100

    products crud +

--- controllers/productCtrl.js
+++ controllers/productCtrl.js
@@ -7,9 +7,52 @@ class APIfeatures {
         this.query = query;
         this.queryString = queryString
     }
-    filtering(){}
-    sorting(){}
-    paginating(){}
+    filtering(){
+
+       
+       
+        const queryObj = {...this.queryString} // queryString = req.query
+        //console.log({hi:this.queryString})
+        //console.log(queryObj)
+        const excludedFields = ["page", "sort", "limit"]
+        excludedFields.forEach(el=>delete(queryObj[el]
+        ))
+       // console.log(queryObj)
+
+        let queryStr  =JSON.stringify(queryObj)      
+        queryStr =queryStr.replace(/\b(gte|gt|lt|lte|regex)\b/g,match => "$" + match)
+       // console.log({after: queryStr})
+        this.query.find(JSON.parse(queryStr))
+        return this
+    }
+
+
+    sorting(){
+
+        if(this.queryString.sort){
+            
+            const sortBy = this.queryString.sort.split(',').join(' ')
+            
+            this.query= this.query.sort(sortBy)
+
+        }else{
+            this.query = this.query.sort("-createdAt")
+        }
+
+        return this
+
+
+
+    }
+    paginating(){
+
+        const page = this.queryString.page * 1 || 1
+        const limit = this.queryString.limit * 1 || 9
+        const skip = (page-1) * limit
+        this.query = this.query.skip(skip).limit(limit)
+
+        return this
+    }
 }
 
 
@@ -24,10 +67,17 @@ const productCtrl = {
 
        try{
 
+        
+        const features = new APIfeatures(Products.find(),req.query).filtering().sorting().paginating()
+        
+        const products = await features.query
+        
 
-        const products = await Products.find()
-
-        res.json(products)
+        res.json({
+            status:"success",
+            result: products.length,
+            products: products
+        })
 
        }catch(err){
 
--- models/productModel.js
+++ models/productModel.js
@@ -65,7 +65,7 @@ const productSchema = new mongoose.Schema({
 
 
 
-})
+},{timestamps:true})
 
 
 module.exports = mongoose.model("Products" , productSchema)
