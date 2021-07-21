78b119dab8d383859607f9fa987b6e0bc6d08960
hichemsamti <samtihichem@gmail.com>
2021-07-21 16:34:11 +0100

    front end routing

--- client/src/components/mainpages/Pages.js
+++ client/src/components/mainpages/Pages.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+import Products from "./products/Products"
+import Login from "./auth/Login"
+import Register from "./auth/Register"
+import Cart from "./cart/Cart"
+import NotFound from "./utils/notFound/NotFound"
 
 export default function Pages() {
     return (
-        <div>
-            Main Pages Components
-        </div>
+        <Switch>
+            <Route path="/" exact component={Products}/>
+            <Route path="/login" exact component={Login}/>
+            <Route path="/register" exact component={Register}/>
+            <Route path="/cart" exact component={Cart}/>
+            
+            <Route path="*" exact component={NotFound}/>
+        </Switch>
     )
 }
 
