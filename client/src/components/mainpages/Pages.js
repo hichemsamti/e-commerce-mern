644abf34658773e17cea3f31c91537356aa23f36
hichemsamti <samtihichem@gmail.com>
2021-07-23 13:19:55 +0100

    products css

--- client/src/components/mainpages/Pages.js
+++ client/src/components/mainpages/Pages.js
@@ -5,11 +5,13 @@ import Login from "./auth/Login"
 import Register from "./auth/Register"
 import Cart from "./cart/Cart"
 import NotFound from "./utils/notFound/NotFound"
+import DetailProduct from "./detailProduct/DetailProduct"
 
 export default function Pages() {
     return (
         <Switch>
             <Route path="/" exact component={Products}/>
+            <Route path="/detail/:id" exact component={DetailProduct}/>
             <Route path="/login" exact component={Login}/>
             <Route path="/register" exact component={Register}/>
             <Route path="/cart" exact component={Cart}/>
--- client/src/components/mainpages/products/Products.js
+++ client/src/components/mainpages/products/Products.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import {GlobalState} from "../../../GlobalState"
+import ProductItem from "../utils/productItem/ProductItem"
+import "./products.css"
+
 
 export default function Products() {
+   const state = useContext(GlobalState)
+   const [products] = state.productsAPI.products
+
+    console.log(products)
     return (
-        <div>
-            Products components
+        <div className="products">
+            {
+                products.map(product => {
+                    return <ProductItem key={product._id} product={product} />
+                })
+            }
+           
         </div>
     )
 }
