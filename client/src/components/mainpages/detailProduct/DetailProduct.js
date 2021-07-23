cdb4f9bfa4848eff6dbc44b7bfb4e3cf0b5b384b
hichemsamti <samtihichem@gmail.com>
2021-07-23 21:36:02 +0100

    product detail component

--- client/src/components/mainpages/detailProduct/DetailProduct.js
+++ client/src/components/mainpages/detailProduct/DetailProduct.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import React ,{useContext, useState, useEffect} from 'react'
+import {useParams, Link} from "react-router-dom"
+import {GlobalState} from "../../../GlobalState"
+import "./productDetail.css"
 
-export default function detailProduct() {
+
+export default function DetailProduct() {
+    const params = useParams()
+    const state = useContext(GlobalState)
+    const [products]=state.productsAPI.products
+    const [detailProduct, setDetailProduct] = useState([])
+    console.log(params)
+
+    useEffect(()=>{
+        if(params)(
+            products.forEach(product =>{
+                if(product._id === params.id ) setDetailProduct(product)
+            })
+        )
+
+    },[params,products])
+
+    console.log(detailProduct)
+    if(detailProduct.length ===0) return null
     return (
-        <div>
-            Detail component
+        <div className="detail">
+            <img src={detailProduct.images.url} alt=""/>
+
+           <div className="box-detail">
+                <div className="row">
+                    <h2>{detailProduct.title}</h2>
+                    <h6>{detailProduct.product_id}</h6>
+
+
+                </div>
+                <span>$ {detailProduct.price}</span>
+                <p> {detailProduct.description}</p>
+                <p>{detailProduct.content}</p>
+                <p> sold:{detailProduct.sold}</p>
+                <Link to="/cart" className="cart"> Buy Now</Link>
+                
+           </div>
+
         </div>
     )
 }
