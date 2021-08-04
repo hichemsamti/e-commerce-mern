468eb6dc65906ad36a0e8f532d3e5c4dabf19c0a
hichemsamti <samtihichem@gmail.com>
2021-08-04 21:48:28 +0100

    category component

--- client/src/api/CategoryAPI.js
+++ client/src/api/CategoryAPI.js
@@ -4,7 +4,7 @@ import axios from "axios"
 
 export default function CategoryAPI() {
     const [categories , setCategories]= useState([])
-
+    const [callback, setCallback ] = useState(false)
     useEffect(()=>{
         const getCategories = async() =>{
             const res =await axios.get('/api/category')
@@ -15,10 +15,12 @@ export default function CategoryAPI() {
         }
 
         getCategories()
-    },[])
+    },[callback])
     return {
 
-        categories : [categories , setCategories]
+        categories : [categories , setCategories],
+
+        callback:[callback, setCallback]
 
 
     }
--- client/src/components/mainpages/categories/Categories.js
+++ client/src/components/mainpages/categories/Categories.js
@@ -1,15 +1,35 @@
 import React,{useState, useContext} from 'react'
 import {GlobalState} from "../../../GlobalState"
+import "./categories.css"
+import axios from "axios"
 
 export default function Categories() {
 
     const state = useContext(GlobalState)
     const [categories , setCategories] = state.categoriesAPI.categories
     const [category, setCategory] = useState("")
+    const [token] = state.token
+    const [callback, setCallback] = state.categoriesAPI.callback
+
+    const createCategory = async e =>{
+        e.preventDefault()
+
+        try{
+            const res= await axios.post("/api/category" ,{name:category},{
+                headers:{Authorization: token}
+            })
+            setCategory('')
+            setCallback(!callback)
+            alert(res.data.msg)
+        }catch(err){
+          //  console.log(err)
+            alert(err.response.data.msg)
+        }
+    }
     return (
         <div className="categories">
 
-            <form>
+            <form onSubmit={createCategory}>
                 <label htmlFor="category">Category</label>
                 <input type="text" name="category" value={category} required
                 onChange={e => setCategory(e.target.value)} />
